2cb5bf1cca345cef9cae860dc63c007c35c916cc
Abhishek Anand <anand2000ys@gmail.com>
2023-04-08 12:18:43 +0530

    socket-io config

--- server.js
+++ server.js
@@ -13,3 +13,10 @@ app.use(express.static(__dirname + "/public")); //middleware express
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+//Socket
+
+const io = require("socket.io")(http);
+io.on("connection", (socket) => {
+  console.log("Connected....");
+});
